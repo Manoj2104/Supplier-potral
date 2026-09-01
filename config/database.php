a1f44ee41ec1db95ce9aff33e87e52e50540757e
Manoj S <manoj2104s@gmail.com>
2026-09-01 18:59:19 +0530

    fix(db): support DB_SSLMODE environment variable for cloud postgresql

--- config/database.php
+++ config/database.php
@@ -88,7 +88,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'central_supabase' => [
